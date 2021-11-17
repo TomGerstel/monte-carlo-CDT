a2d9589aaf1e58248cd5e4ce409b4d32f3b74abe
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2021-11-17 15:47:32 +0100

    added command line input

--- job.sh
+++ job.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem=100M
 #SBATCH --time=2:00:00
 cd ~/Documents/monte-carlo-CDT
-./target/release/monte-carlo-cdt
+./target/release/monte-carlo-cdt -a=7 -b=5
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use structopt::StructOpt;
+
+/// A test to add two numbers
+#[derive(StructOpt, Debug)]
+#[structopt(name = "add_test")]
+struct Opt {
+    ///First number to add
+    #[structopt(short, long)]
+    alpha: usize,
+
+    ///Second number to add
+    #[structopt(short, long)]
+    beta: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    let a = opt.alpha;
+    let b = opt.beta;
+    println!("{:#?}", a + b);
 }
