6638a8192392b2884952b467f65c2c66ca4be944
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2021-11-17 16:18:37 +0100

    add dot

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=hefstud
-#SBATCH --output=/cluster_output/std_%A_%a.txt
+#SBATCH --output=./cluster_output/std_%A_%a.txt
 #SBATCH --mem=100M
 #SBATCH --time=2:00:00
 cd ~/Documents/monte-carlo-CDT
