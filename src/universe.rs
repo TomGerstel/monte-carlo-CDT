cdf62a2131a256899149c60aea9326210a096729
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2021-11-27 13:47:57 +0100

    simplified vertexpos indexing

--- src/universe.rs
+++ src/universe.rs
@@ -14,27 +14,21 @@ struct Vertex {
 
 #[derive(Copy, Clone, Debug)]
 struct VertexPos {
-    time: Time,
-    space: Space,
+    time: usize,
+    space: usize,
 }
-
-#[derive(Copy, Clone, Debug)]
-struct Time(usize);
-
-#[derive(Copy, Clone, Debug)]
-struct Space(usize);
 
 impl Index<VertexPos> for Universe {
     type Output = Vertex;
 
     fn index(&self, pos: VertexPos) -> &Self::Output {
-        &self.slices[pos.time.0][pos.space.0]
+        &self.slices[pos.time][pos.space]
     }
 }
 
 impl IndexMut<VertexPos> for Universe {
     fn index_mut(&mut self, pos: VertexPos) -> &mut Self::Output {
-        &mut self.slices[pos.time.0][pos.space.0]
+        &mut self.slices[pos.time][pos.space]
     }
 }
 
@@ -52,14 +46,8 @@ impl Universe {
             order_four: vec![],
         };
         for t in 0..max_time {
-            let pos_next = VertexPos {
-                time: Time((t + 1) % max_time),
-                space: Space(0),
-            };
-            let pos_prev = VertexPos {
-                time: Time((t + max_time - 1) % max_time),
-                space: Space(0),
-            };
+            let pos_next = VertexPos::new((t + 1) % max_time, 0);
+            let pos_prev = VertexPos::new((t + max_time - 1) % max_time, 0);
             let vertex = Vertex {
                 neighbours_next: vec![pos_next, pos_next],
                 neighbours_prev: vec![pos_prev, pos_prev],
@@ -69,14 +57,8 @@ impl Universe {
         }
 
         let n_free = n_vertex - max_time;
-        let pos_next = VertexPos {
-            time: Time(1),
-            space: Space(0),
-        };
-        let pos_prev = VertexPos {
-            time: Time(max_time - 1),
-            space: Space(0),
-        };
+        let pos_next = VertexPos::new(1, 0);
+        let pos_prev = VertexPos::new(max_time - 1, 0);
         for s in 0..n_free {
             let vertex = Vertex {
                 neighbours_next: vec![pos_next],
@@ -84,14 +66,17 @@ impl Universe {
             };
             universe.slices[0].push(vertex);
 
-            let pos = VertexPos {
-                time: Time(0),
-                space: Space(s + 1),
-            };
+            let pos = VertexPos::new(0, s + 1);
             universe.order_four.push(pos);
             universe[pos_next].neighbours_prev.push(pos);
             universe[pos_prev].neighbours_next.push(pos);
         }
         universe
+    }
+}
+
+impl VertexPos {
+    fn new(time: usize, space: usize) -> Self {
+        VertexPos { time, space }
     }
 }
