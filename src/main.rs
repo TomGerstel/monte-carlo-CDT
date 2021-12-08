d8c527f115bed7d2bcf4e95032590bb98fe51a4b
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2021-12-08 19:56:56 +0100

    commandline interface

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,73 @@ use structopt::StructOpt;
 mod universe;
 pub use universe::*;
 
-/// A test to add two numbers
+/// A Markov Chain Monte Carlo simulation of 2-dimensional Causal Dynamical Triangulations.
 #[derive(StructOpt, Debug)]
-#[structopt(name = "add_test")]
+#[structopt(name = "monte_carlo_CDT")]
 struct Opt {
-    /// First number to add
-    #[structopt(short, long)]
-    alpha: usize,
+    /// Number of timeslices
+    #[structopt(short = "t", long)]
+    timespan: usize,
 
-    /// Second number to add
-    #[structopt(short, long)]
-    beta: usize,
+    /// Number of triangles
+    #[structopt(short = "n", long)]
+    triangle_count: usize,
+
+    /// Probability of performing a shard move for a single Markov chain step
+    /// in the equilibration phase
+    #[structopt(short = "b", long)] // TODO: fill in default value when we know optimal value
+    move_ratio_eq: f32,
+
+    /// Probability of performing a shard move for a single Markov chain step
+    /// in the measurement phase
+    #[structopt(short = "r", long)] // TODO: fill in default value when we know optimal value
+    move_ratio_meas: f32,
+
+    /// Length of equilibration phase in sweeps
+    #[structopt(short = "e", long)]
+    eq_sweeps: usize,
+
+    /// Length of measurement phase in sweeps
+    #[structopt(short = "m", long)]
+    meas_sweeps: usize,
 }
 
+// example command (on Windows):
+// target\release\monte-carlo-cdt.exe -t 20 -n 800 -b 0.7 -r 0.5 -e 10 -m 100
 fn main() {
-    //let opt = Opt::from_args();
-    //let a = opt.alpha;
-    //let b = opt.beta;
-    //println!("{:#?}", a + b);
+    // set parameters
+    let opt = Opt::from_args();
+    let timespan = opt.timespan;
+    let triangle_count = opt.triangle_count;
+    let move_ratio_eq = opt.move_ratio_eq;
+    let move_ratio_meas = opt.move_ratio_meas;
+    let eq_sweeps = opt.eq_sweeps;
+    let meas_sweeps = opt.meas_sweeps;
 
-    let mut universe = universe::Universe::new(20, 800);
-    for _ in 0..500_000 {
-        universe.mcmc_step(0.5);
+    // check move ratio parameters
+    assert!(
+        (0.0..=1.0).contains(&move_ratio_eq),
+        "given move ratio ({}) is outside valid range [0.0, 1.0]",
+        move_ratio_eq
+    );
+    assert!(
+        (0.0..=1.0).contains(&move_ratio_meas),
+        "given move ratio ({}) is outside valid range [0.0, 1.0]",
+        move_ratio_meas
+    );
+
+    // define sweeps
+    let sweep = triangle_count / 2;
+
+    // equilibration phase
+    let mut universe = universe::Universe::new(timespan, triangle_count);
+    for _ in 0..(eq_sweeps * sweep) {
+        universe.mcmc_step(move_ratio_eq);
+    }
+
+    // measurement phase
+    for _ in 0..(meas_sweeps * sweep) {
+        universe.mcmc_step(move_ratio_meas);
+        // TODO: do measurments here
     }
 }
