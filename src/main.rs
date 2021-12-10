dfe989132443ca9f3acf6e1e188b493914883cae
Jesse van der Duin <jesse.vincitech@gmail.com>
2021-12-10 18:13:53 +0100

    Did preliminary data-analysis added simple CLI for initial analysis

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use structopt::StructOpt;
 mod universe;
-pub use universe::*;
 
 /// A Markov Chain Monte Carlo simulation of 2-dimensional Causal Dynamical Triangulations.
 #[derive(StructOpt, Debug)]
@@ -33,9 +32,64 @@ struct Opt {
     meas_sweeps: usize,
 }
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "monte_carlo_CDT")]
+struct OptSimple {
+    /// Number of timeslices
+    #[structopt(short = "t", long)]
+    timespan: usize,
+
+    /// Number of triangles
+    #[structopt(short = "n", long)]
+    triangle_count: usize,
+
+    /// Probability of performing a shard move for a single Markov chain step
+    /// in the equilibration phase
+    #[structopt(short = "r", long)] // TODO: fill in default value when we know optimal value
+    move_ratio: f32,
+
+    /// Amount of Markov Chain Monte Carlo steps
+    #[structopt(short = "s", long)]
+    steps: usize,
+}
+
 // example command (on Windows):
 // target\release\monte-carlo-cdt.exe -t 20 -n 800 -b 0.7 -r 0.5 -e 10 -m 100
 fn main() {
+    simple_measurement();
+}
+
+fn simple_measurement() {
+    // A simpler CLI for preliminary data-analysis which simply performs the model for a given amount of steps
+    
+    // set parameters
+    let opt = OptSimple::from_args();
+    let timespan = opt.timespan;
+    let triangle_count = opt.triangle_count;
+    let move_ratio = opt.move_ratio;
+    let steps = opt.steps;
+
+    // check move ratio parameters
+    assert!(
+        (0.0..=1.0).contains(&move_ratio),
+        "given move ratio ({}) is outside valid range [0.0, 1.0]",
+        move_ratio
+    );
+
+    // measurements
+    let mut universe = universe::Universe::new(timespan, triangle_count);
+    for _ in 0..steps {
+        universe.mcmc_step(move_ratio);
+        for length in universe.lengths(0) {
+            print!("{} ", length);
+        }
+        println!("");
+        
+    }
+}
+
+
+fn full_measurement() {
     // set parameters
     let opt = Opt::from_args();
     let timespan = opt.timespan;
