726129ce83027f91ba9eccd5d71bfcd23bea9db0
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2022-01-06 13:19:49 +0100

    job script

--- job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=hefstud
-#SBATCH --output=./cluster_output/std_%A_%a.txt
-#SBATCH --mem=100M
-#SBATCH --time=2:00:00
-cd ~/Documents/monte-carlo-CDT
-./target/release/monte-carlo-cdt -a=7 -b=5
---
+++ job_tom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=hefstud
+#SBATCH --output=./cluster_output/std_%A_%a.txt
+#SBATCH --mem=100M
+#SBATCH --time=3:00:00
+#SBATCH --array=0-10%3
+cd ~/Documents/monte-carlo-CDT
+index={0..10}
+i=${index[$SLURM_ARRAY_TASK_ID]}
+T=$((10+10*$i))
+L=$(200)
+tcor=$((4*$T))
+./target/release/monte-carlo-cdt -m -t $T -l $L -n 100 -r 0.4 -b 200 -p $tcor -o "./data"
