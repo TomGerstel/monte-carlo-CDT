03d48bae87c6a0e3bf26bac9a08d4871ba57a8f8
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2022-01-06 13:32:39 +0100

    dollar sign

--- job_tom.sh
+++ job_tom.sh
@@ -5,6 +5,6 @@
 #SBATCH --time=3:00:00
 #SBATCH --array=1-10%3
 cd ~/Documents/monte-carlo-CDT
-T=$((10*$SLURM_ARRAY_TASK_ID))
+T=$((10*SLURM_ARRAY_TASK_ID))
 tcor=$((4*$T))
 ./target/release/monte-carlo-cdt -m -t $T -l 200 -n 100 -r 0.4 -b 200 -p $tcor -o "./data"
