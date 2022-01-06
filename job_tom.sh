bb4202b8f8367d74ca6c5e790ac89c2e4a5171c8
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2022-01-06 13:51:50 +0100

    test

--- job_tom.sh
+++ job_tom.sh
@@ -3,11 +3,10 @@
 #SBATCH --output=./cluster_output/std_%A_%a.txt
 #SBATCH --mem=100M
 #SBATCH --time=3:00:00
-#SBATCH --array=0-10%3
+#SBATCH --array=0-9%3
 cd ~/Documents/monte-carlo-CDT
-index={0..10}
-i=${index[$SLURM_ARRAY_TASK_ID]}
-T=$((100+20*$i))
-L=$((80+20*$i))
-tcor=$((4*$T))
-./target/release/monte-carlo-cdt -m -t $T -l $L -n 100 -r 0.4 -b 200 -p $tcor -o "./data"
+Ts=($(LANG=en_US seq 10 10 100))
+T=${Ts[$SLURM_ARRAY_TASK_ID]}
+tcors=($(LANG=en_US seq 40 40 400))
+tcor=${tcors[$SLURM_ARRAY_TASK_ID]}
+./target/release/monte-carlo-cdt -m -t ${T} -l 200 -n 100 -r 0.4 -b 200 -p ${tcor} -o "./data"
