4bb3fdd87229a617ef74755518146830c3ae9478
SirBlueRabbit <68923537+SirBlueRabbit@users.noreply.github.com>
2022-01-08 12:48:55 +0100

    update large T job

--- largeT.sh
+++ largeT.sh
@@ -5,9 +5,10 @@
 #SBATCH --time=3:00:00
 #SBATCH --array=0-3%4
 cd ~/Documents/monte-carlo-CDT
-index={0..4}
-i=${index[$SLURM_ARRAY_TASK_ID]}
-L=$((15+5*$i))
-T=$((20*$L))
-tcor=$((4*$L))
+Ls=($(LANG=en_US seq 15 5 30))
+Ts=($(LANG=en_US seq 300 100 600))
+tcors=($(LANG=en_US seq 60 20 120))
+L=${Ls[$SLURM_ARRAY_TASK_ID]}
+T=${Ts[$SLURM_ARRAY_TASK_ID]}
+tcor=${tcors[$SLURM_ARRAY_TASK_ID]}
 ./target/release/monte-carlo-cdt -m -t $T -l $L -n 100 -r 0.4 -b 200 -p $tcor -o "./data"
